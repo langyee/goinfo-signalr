f1257724b22d34d59b774d09d764ab7852aea6eb
Mike Liao <mike.hm.liao@gmail.com>
2020-07-31 17:01:46 +0800

    added mongo settings in Data layer

---
+++ Data/MongoSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    class MongoSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
