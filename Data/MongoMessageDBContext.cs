ce93b6930ed4090239d0b3c944c9062617f87343
Mike Liao <mike.hm.liao@gmail.com>
2020-07-31 14:26:22 +0800

    added Data and Shared projects
    moved codes to project based

---
+++ Data/MongoMessageDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    class MongoMessageDBContext
+    {
+        private IMongoDatabase _db { get; set; }
+        private MongoClient _mongoClient { get; set; }
+        public IClientSessionHandle Session { get; set; }
+        //public MongoMessageDBContext(IOptions<MongoSettings> configuration)
+        //{
+
+        //}
+    }
+}
---
+++ Shared/Models/JournalMessage.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models
+{
+    public class JournalMessage
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public int Corpsno { get; set; }
+        public string Corpname { get; set; }
+        public DateTime Sdate { get; set; }
+        public string Serno { get; set; }
+        public string Title { get; set; }
+        public string Sender { get; set; }
+        public int JournalId { get; set; }
+        public string AdditaionalMessage { get; set; }
+    }
+}
